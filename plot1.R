424c840b7383661d4be2b08a748753a7f0b963dd
myheadphones <myheadphones@users.noreply.github.com>
2014-12-07 16:27:04 -0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,14 @@
+# read in the data
+power <- read.csv("household_power_consumption.txt", sep=";", na.strings = "?")
+power2 <- tbl_df(power)
+power3 <- filter(power2,Date == "1/2/2007" | Date == "2/2/2007")
+
+#set it up for 1 plot
+par(mfcol = c(1,1))
+
+# plot it
+hist(power3$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+
+# Save the plot
+dev.print(device = png, width = 480, height = 480, filename = "plot1.png")
+dev.off()
